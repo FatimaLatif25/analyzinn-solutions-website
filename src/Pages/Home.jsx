576ab0104e450038e4f69b78c4c33313068953ac
Fatima Latif <167830068+FatimaLatif25@users.noreply.github.com>
2024-08-06 17:52:56 +0500

    hero section layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import HeroSection from '../Components/HeroSection/HeroSection'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
       </>
   )
 }
